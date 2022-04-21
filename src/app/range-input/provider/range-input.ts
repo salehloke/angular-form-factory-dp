55d5fb709dc7a2c6de976ce5d1e102f3a4a2472a
salehloke <salehloke@gmail.com>
2022-04-22 00:39:30 +0800

    3 files added

---
+++ src/app/range-input/provider/range-input.ts
@@ -0,0 +1,26 @@
+import { FormGroup } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { distinctUntilChanged, map, startWith } from 'rxjs/operators';
+
+export class RangeInput {
+  readonly initialValue;
+
+  constructor(private formGroup: FormGroup) {
+    this.initialValue = formGroup.value;
+  }
+
+  get asFormGroup() {
+    return this.formGroup;
+  }
+
+  isValid(): Observable<boolean> {
+    return this.formGroup.statusChanges.pipe(
+      map(() => this.formGroup.valid),
+      startWith(false)
+    );
+  }
+
+  reset() {
+    this.formGroup.reset();
+  }
+}
---
+++ src/app/range-input/range-input.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ip-range-input',
+  templateUrl: './range-input.component.html',
+  styleUrls: ['./range-input.component.css'],
+})
+export class RangeInputComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
